0f8208dc65504c11091a4d61ce2162acc73a4548
nu11secur1ty <venvaropt@gmail.com>
2024-03-12 10:15:29 +0200

    Update G0BurpSQLmaPI.go

--- program/Framework-Advanced-Auto/G0BurpSQLmaPI.go
+++ program/Framework-Advanced-Auto/G0BurpSQLmaPI.go
@@ -18,7 +18,7 @@ func main() {
     defer f.Close()
 
 	// The start of your POST or GET or whatever VULNERABLE request...
-    val := ``
+    val := `your_POST_or_GET_Request_from_Burp_Suite_here`
 	// The end of your POST or GET or whatever VULNERABLE request...
 
     data := []byte(val)
