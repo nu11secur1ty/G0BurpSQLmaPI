1c3d0ccf18f8ee7b92b29faf848449f7ae3ebdde
nu11secur1ty <venvaropt@gmail.com>
2024-05-05 14:20:46 +0300

    Update G0BurpSQLmaPIrun.py

--- program/Framework-Advanced-Auto/G0BurpSQLmaPIrun.py
+++ program/Framework-Advanced-Auto/G0BurpSQLmaPIrun.py
@@ -2,9 +2,31 @@
 import os
 import time
 
-os.system("go run G0BurpSQLmaPI.go")
-time.sleep(5)
-os.system("python G0BurpSQLmaPI.py")
-time.sleep(5)
-os.system("go run executor.go")
-print("Done: you've cleaned the evidence!")
+
+def display_menu():
+    print("===== G0BurpSQLmaPI Menu =====")
+    print("1. Generate PoC")
+    print("2. Start PyG0BurpSQLmaPI")
+    print("3. Clean")
+    print("4. Exit")
+
+def execute_command(choice):
+    if choice == '1':
+        os.system("go run G0BurpSQLmaPI.go")
+    elif choice == '2':
+        os.system("python G0BurpSQLmaPI.py")
+    elif choice == '3':
+        os.system("go run executor.go")
+        print("Done: you've cleaned the evidence!")
+    elif choice == '4':
+        print("Exiting...")
+    else:
+        print("Invalid choice. Please select a valid option.")
+if __name__ == "__main__":
+    while True:
+        display_menu()
+        user_choice = input("Enter your choice: ")
+        execute_command(user_choice)
+        
+        if user_choice == '4':
+            break
